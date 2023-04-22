bbc3e16ef17d3a1a6160b859682e8a4ef428b313
zaindeen <m.s.zainulabideen@gmail.com>
2023-04-22 06:09:42 +0530

    assn_2

--- js/features.js
+++ js/features.js
@@ -7,38 +7,7 @@ tvshows_ep_drop.forEach(
     drop => {drop.addEventListener('click',onWClick)}
 );
 click = [0,0,0];
-totalTime = [0,0,0];
-let prev = [[0,0],[0,0],[0,0]];
 
-// creating total time watched
-const timeBox  = document.createElement('div');
-const timeText = document.createElement('div');
-const time  = document.createElement('div');
-
-//class names
-timeBox.classList.add('timeBox');
-timeText.classList.add('text');
-time.classList.add('total_time');
-//values
-timeText.innerHTML='Total Time Spent'
-time.textContent = '0 days, 0 hours, 0 mins'
-//appending
-const container = document.querySelector('.container');
-container.appendChild(timeBox);
-timeBox.appendChild(timeText);
-timeBox.appendChild(time);
-
-// calculating total time
-function compute(){
-    var total = totalTime[0]+totalTime[1]+totalTime[2];
-    var day = Math.floor(total/1440);
-    total%=1440;
-    var hour = Math.floor(total/60);
-    total%=60;
-    var min = Math.floor(total);
-    time.innerHTML='';
-    time.textContent= day+' days, '+hour+' hours, '+min+' mins';
-}
 
 function checkAll(event){
     const present = event.currentTarget;
@@ -73,28 +42,6 @@ function checkAll(event){
     }
 }
 
-function calTime(event){
-    const element = event.currentTarget;
-    const showNum = element.classList[1].split('_')[2];
-    const seasonNum = element.id.split('_')[2];
-    const epiNum = element.id.split('_')[3];
-    if(element.dataset.watched == 'true')
-        totalTime[showNum] += TV_SHOWS[showNum].seasons[seasonNum-1][epiNum].runtime;
-    else
-        totalTime[showNum] -= TV_SHOWS[showNum].seasons[seasonNum-1][epiNum].runtime;
-    compute();
-}
-
-function calTimeAll(elementAll){
-    const showNum = elementAll.classList[1].split('_')[2];
-    const seasonNum = elementAll.id.split('_')[2];
-    const epiNum = elementAll.id.split('_')[3];
-    if(elementAll.dataset.watched == "true")
-        totalTime[showNum] += TV_SHOWS[showNum].seasons[seasonNum-1][epiNum].runtime;
-    else
-        totalTime[showNum] -= TV_SHOWS[showNum].seasons[seasonNum-1][epiNum].runtime;
-    compute();
-}
 
 function toggle(event){
     const ele = event.currentTarget;
